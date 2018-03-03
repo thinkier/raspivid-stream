2d2b0d45e42a2eb6a390b307e8a044c19713428f
thinkier <26728734+thinkier@users.noreply.github.com>
2018-03-03 23:14:33 +1100

    officially giving up

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn main() {
 	env_logger::init();
 	clean_tmp_dir();
 
-	thread::spawn(|| {
+	thread::spawn(|| { // TODO Migrate to serving jpegs in 50ms intervals, because there is no way serving video is feasible without that FUCKING WHITE FLASH
 		let mut iron = Iron::new(|req: &mut Request| Ok(match req.url.path().pop().unwrap_or("") {
 			"" => {
 				// Serve the script with html
