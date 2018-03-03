0848fe4f6e9e1ff03a8a283cafd2b06ae5260831
thinkier <26728734+thinkier@users.noreply.github.com>
2018-03-03 19:54:59 +1100

    sleeper is broken, simple api

--- src/main.rs
+++ src/main.rs
@@ -208,7 +208,7 @@ impl FFMpeg {
 			.args(vec!["-i", "-"]) // Bind to STDIN
 			.args(vec!["-c:v", "copy"]) // Copy video only
 			.args(vec!["-f", "mp4"]) // Output as mp4
-			.args(vec!["-movflags", "empty_moov"]) // Empty moov so that actually idk what this does lmao
+			.args(vec!["-movflags", "faststart"]) // Mov data at the start for faster loading
 			.arg(&format!("{}/stream_replace.mp4", STREAM_TMP_DIR))
 			.stdin(process::Stdio::piped())
 			.stdout(process::Stdio::null())
