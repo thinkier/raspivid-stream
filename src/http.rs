7251d08833d191be1af1c89fe2418339625d9b5e
thinkier <26728734+thinkier@users.noreply.github.com>
2018-03-05 20:36:37 +1100

    state indicator

--- src/http.rs
+++ src/http.rs
@@ -30,10 +30,12 @@ pub fn init_iron() {
 						newStreamer.src = '/' + num;
 						newStreamer.style = 'width:100%;height:auto;display:none;';
 						newStreamer.onplay = function() {
+						document.body.style.backgroundColor = 0x00FF00;
 							streamer.parentNode.removeChild(streamer);
 							newStreamer.style.display = 'inline';
 							register(newStreamer, num + 1);
 						};
+						document.body.style.backgroundColor = 0xFF0000;
 					}
 				}
 				")));
